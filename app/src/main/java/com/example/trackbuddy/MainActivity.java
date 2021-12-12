52a577d9b26575cd4a1a76ac6c59f3010d9eb395
hackeramitkumar <amit9116260192@gmail.com>
2021-12-12 14:45:33 +0530

    This is the first commit to the repo

---
+++ app/src/main/java/com/example/trackbuddy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.trackbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton fb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Calender"));
+        tabLayout.addTab(tabLayout.newTab().setText("ToDo"));
+        tabLayout.addTab(tabLayout.newTab().setText("Notes"));
+
+
+
+        final MainAdapter adapter = new MainAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+        fb = findViewById(R.id.button);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+}
+
+
+
+
